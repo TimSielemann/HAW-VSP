7652249c5711e7a36b3a7f545cb5938e6a2b6f65
Amin Nabouli <amin.nabouli@haw-hamburg.de>
2015-05-29 19:23:42 +0200

    accessor_one/two hinzugefuegt

---
+++ VSP3/src/accessor_one/SomeException112.java
@@ -0,0 +1,16 @@
+package accessor_one;
+
+
+
+public class SomeException112 extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3335529187241412033L;
+	
+		public SomeException112(String message){
+			super(message);
+		}
+
+}
