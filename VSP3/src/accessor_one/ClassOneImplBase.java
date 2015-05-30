05bd690e5bb1313c86a85b366ea0ebdc833869ea
Amin Nabouli <amin.nabouli@haw-hamburg.de>
2015-05-30 15:13:54 +0200

    accessoren um die Stubs erweitert

--- VSP3/src/accessor_one/ClassOneImplBase.java
+++ VSP3/src/accessor_one/ClassOneImplBase.java
@@ -1,17 +1,12 @@
 package accessor_one;
 
-
-
 public abstract class ClassOneImplBase {
 
-	public static String methodOne(String param1, int param2) throws SomeException112 {
-		Kommunikationsmodul.send()
-		return null;
+	public abstract String methodOne(String param1, int param2)
+			throws SomeException112;
+
+	public static ClassOneImplBase narrowCast(Object rawObjectRef) {
+		return new ClassOneImplBaseStub(rawObjectRef);
 	}
-	
-	public static ClassOneImplBase narrowCast(Object rawObjectRef){
-		return null;
-	}
-	
 
 }
--- VSP3/src/accessor_one/ClassTwoImplBase.java
+++ VSP3/src/accessor_one/ClassTwoImplBase.java
@@ -1,19 +1,13 @@
 package accessor_one;
 
-
-
 public abstract class ClassTwoImplBase {
-	public int methodOne(double param1) throws SomeException110{
-		return 0;
-		
-	}
-	public double methodTwo() throws SomeException112{
-		return 0;
-		
-	}
-	public static ClassTwoImplBase narrowCast(Object rawObjectRef){
-		return null;
-		
+	public abstract int methodOne(double param1) throws SomeException110;
+
+	public abstract double methodTwo() throws SomeException112;
+
+	public static ClassTwoImplBase narrowCast(Object rawObjectRef) {
+		return new ClassTwoImplBaseStub(rawObjectRef);
+
 	}
 
 }
